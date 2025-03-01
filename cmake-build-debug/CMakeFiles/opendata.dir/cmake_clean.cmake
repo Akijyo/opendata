16d7895166960dd301e816b1a6d353e9498021b2
Akijyo <2825377856@qq.com>
2025-03-01 17:02:10 +0800

    “改用makefile”

---
+++ cmake-build-debug/CMakeFiles/opendata.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opendata.dir/main.cpp.o"
+  "CMakeFiles/opendata.dir/main.cpp.o.d"
+  "opendata"
+  "opendata.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/opendata.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/opendata.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for opendata.
+# This may be replaced when dependencies are built.
