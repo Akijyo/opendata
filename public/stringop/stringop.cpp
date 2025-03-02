58d57ab8016d5b1da3aff2b0e0139df5cd94e11f
Akijyo <2825377856@qq.com>
2025-03-02 10:00:35 +0800

    2025/3/2 morning

--- public/stringop/stringop.cpp
+++ public/stringop/stringop.cpp
@@ -12,10 +12,19 @@ char* deleteLChar(char* str, const char ch)
     {
         return nullptr;
     }
-    while (*str==ch)//跳过左侧的指定字符
+    char* readPtr=str;
+    char* writePtr=str;
+    while (*readPtr==ch)//跳过左侧的指定字符
     {
-        str++;
+        readPtr++;
     }
+    while (*readPtr!='\0')//复制剩余的字符串
+    {
+        *writePtr=*readPtr;
+        writePtr++;
+        readPtr++;
+    }
+    *writePtr='\0';
     return str;
 }
 //删除字符串左侧的指定字符,C++版本
@@ -25,9 +34,14 @@ string& deleteLChar(string& str, const char ch)
     {
         return str;
     }
-    while (str[0]==ch)//跳过左侧的指定字符
+    int start = 0;
+    while(start<str.length()&&str[start]==ch)//跳过左侧的指定字符
     {
-        str.erase(0,1);
+        start++;
+    }
+    if(start>0)
+    {
+        str.erase(0, start);
     }
     return str;
 }
@@ -53,11 +67,121 @@ string& deleteRChar(string& str, const char ch)
     {
         return str;
     }
-    int len=str.length();
-    while (str[len-1]==ch)//跳过右侧的指定字符
+    int end = str.length() - 1;
+    while(end>=0&&str[end]==ch)//跳过右侧的指定字符
     {
-        str.erase(len-1,1);
+        end--;
+    }
+    if(end<str.length()-1)
+    {
+        str.resize(end+1);
     }
     return str;
 }
 //删除字符串两侧的指定字符,C语言版本
+char* deleteChar(char* str,const char ch)
+{
+    if(str==nullptr)//传入的字符串为空
+    {
+        return nullptr;
+    }
+    str=deleteLChar(str,ch);//删除左侧的指定字符
+    str=deleteRChar(str,ch);//删除右侧的指定字符
+    return str;
+}
+//删除字符串两侧的指定字符,C++版本
+string& deleteChar(string& str,const char ch)
+{
+    if(str.empty())
+    {
+        return str;
+    }
+    str=deleteLChar(str,ch);//删除左侧的指定字符
+    str=deleteRChar(str,ch);//删除右侧的指定字符
+    return str;
+}
+//将字符串中的小写字母转换为大写字母,C语言版本
+char* toUpper(char* str)
+{
+    if(str==nullptr)//传入的字符串为空
+    {
+        return nullptr;
+    }
+    char *ptr = str;
+    while(*ptr!='\0')
+    {
+        if(*ptr>='a'&&*ptr<='z')//小写字母转换为大写字母
+        {
+            *ptr=*ptr-32;
+        }
+        ptr++;
+    }
+    return str;
+}
+//将字符串中的小写字母转换为大写字母,C++版本
+string& toUpper(string& str)
+{
+    if(str.empty())//传入的字符串为空
+    {
+        return str;
+    }
+    for(auto& ch:str)
+    {
+        if(ch>='a'&&ch<='z')//小写字母转换为大写字母
+        {
+            ch=ch-32;
+        }
+    }
+    return str;
+}
+//将字符串中的大写字母转换为小写字母,C语言版本
+char* toLower(char* str)
+{
+    if(str==nullptr)//传入的字符串为空
+    {
+        return nullptr;
+    }
+    char *ptr = str;
+    while(*ptr!='\0')
+    {
+        if(*ptr>='A'&&*ptr<='Z')//大写字母转换为小写字母
+        {
+            *ptr=*ptr+32;
+        }
+        ptr++;
+    }
+    return str;
+}
+//将字符串中的大写字母转换为小写字母,C++版本
+string& toLower(string& str)
+{
+    if(str.empty())//传入的字符串为空
+    {
+        return str;
+    }
+    for(auto& ch:str)
+    {
+        if(ch>='A'&&ch<='Z')//大写字母转换为小写字母
+        {
+            ch=ch+32;
+        }
+    }
+    return str;
+}
+//将目标字符串str中的src字符串替换为dest字符串,C语言版本
+bool replace(char *str, const char *src, const char *dest, const bool isLoop)
+{
+    if(isLoop==true&&strstr(dest,src)!=nullptr)//替换后的字符串中包含被替换的字符串
+    {
+        return false;//循环替换会导致死循环
+    }
+    char *pos = strstr(str, src);
+    if(pos==nullptr)//目标字符串中不包含被替换的字符串
+    {
+        return false;
+    }
+    int srcLen = strlen(src);
+    int destLen = strlen(dest);
+    int strLen = strlen(str);
+    
+}
--- public/stringop/stringop.h
+++ public/stringop/stringop.h
@@ -28,9 +28,38 @@ std::string& deleteRChar(std::string& str,const char ch=' ');
  * 删除字符串左右两边的指定字符，默认缺省为空格
  * @param str 要处理的字符串
  * @param ch 要删除的字符
- * @return char* 处理后的字符串
+ * @return  处理后的字符串
  */
 char* deleteChar(char* str,const char ch=' ');
 std::string& deleteChar(std::string& str,const char ch=' ');
 
+/**
+ * @brief 将字符串中的小写字母转换为大写字母
+ * 
+ * @param str 要处理的字符串
+ * @return 处理完后的字符串
+ */
+char* toUpper(char* str);
+std::string& toUpper(std::string& str);
 
+/**
+ * @brief 将字符串的大写字母全部转换为小写字母
+ * 
+ * @param str 要处理的字符串
+ * @return 处理完后的字符串
+ */
+char *toLower(char *str);
+std::string &toLower(std::string &str);
+
+/**
+ * @brief 将目标字符串str中的src字符串替换为dest字符串
+ * 
+ * @param str 目标字符串
+ * @param src 被替换的字符串
+ * @param dest 需要替换成的字符串
+ * @param isLoop 是否循环替换，默认为false
+ * @return true 
+ * @return false 
+ */
+bool replace(char *str, const char *src, const char *dest, const bool isLoop = false);
+bool replace(std::string &str, const std::string &src, const std::string &dest, const bool isLoop = false);
