77784a78beedfd290caf8147d23b80a97ac456d9
Akijyo <2825377856@qq.com>
2025-03-03 21:47:30 +0800

    20253/3 evening

--- public/stringop/stringop.cpp
+++ public/stringop/stringop.cpp
@@ -3,6 +3,9 @@
 //字符串操作函数的实现
 
 #include "stringop.h"
+#include <cmath>
+#include <cstring>
+#include <string>
 using namespace std;
 
 //删除字符串左侧的指定字符,C语言版本
@@ -249,4 +252,132 @@ bool matchstr(const string &str, const string &pattern)
     const string temp = "R\"(" + pattern + ")\"";
     regex reg(pattern);
     return regex_match(str,reg);
+}
+
+///////////////////////////////////////////
+///////下面为ccmdstr类的实现///////////////
+///////////////////////////////////////////
+
+//分割字符串的方法
+bool ccmdstr::split(const string &str, const string &sep, bool isDeleteSpace)
+{
+    if(str.empty()||sep.empty())//传入的字符串为空
+    {
+        return false;
+    }
+    int index = 0; // 用于遍历的指针
+    int start = 0; // 用于存放子字符串的起始位置
+    string temp;//存放截获下来的临时字符串
+    while ((index = str.find(sep, index)) != string::npos)
+    {
+        temp=str.substr(start,index-start);
+        if(isDeleteSpace==true)
+        {
+            temp=deleteChar(temp);//删除空格
+        }
+        if (isDeleteSpace==true) {
+            deleteChar(temp);
+        }
+        if (temp.empty()==false) {
+            this->vstr.push_back(temp);
+        }
+        index += sep.length();
+        start = index;
+    }
+    temp = str.substr(start);
+    if (isDeleteSpace==true) {
+        deleteChar(temp);
+    }
+    if (temp.empty()==false) {
+        this->vstr.push_back(temp);
+    }
+    return true;
+}
+
+//超级构造函数，在构造期间完成字符串的分割
+ccmdstr::ccmdstr(const string &str, const string &sep, bool isDeleteSpace)
+{
+    this->split(str, sep, isDeleteSpace);
+}
+
+bool ccmdstr::checkIndex(const int index)
+{
+    if (index < 0 || index >= this->vstr.size())//下表越界
+    {
+        return false;
+    }
+    return true;
+}
+
+// 重载[]运算符，返回分割后的字符串,像数组一样访问
+string ccmdstr::operator[](const int index)
+{
+    if (index < 0 || index >= this->vstr.size())//下表越界
+    {
+        return string();
+    }
+    return this->vstr[index];
+}
+//返回字符串数组的大小
+int ccmdstr::size()
+{
+    return this->vstr.size();
+}
+// 获取分割后字符串的对应类型的值，需要指定下表
+bool ccmdstr::getValue(const int index, string &value)
+{
+    this->checkIndex(index);
+    value = this->vstr[index];
+    return true;
+}
+bool ccmdstr::getValue(const int index, char *value, const int len)
+{
+    this->checkIndex(index);
+    if (len <= this->size())
+    {
+        strncpy(value, this->vstr[index].c_str(), this->size());
+    }
+    else
+    {
+        strncpy(value, this->vstr[index].c_str(), len);
+    }
+    return true;
+}
+bool ccmdstr::getValue(const int index, int &value)
+{
+    this->checkIndex(index);
+    try
+    {
+        value = stoi(pickNum(this->vstr[index],true));
+    } catch (const std::exception &e)
+    {
+        return false;
+    }
+    return true;
+}
+bool ccmdstr::getValue(const int index, unsigned int &value)
+{
+    this->checkIndex(index);
+    try
+    {
+        value=stoi(pickNum(this->vstr[index]));
+    }
+    catch (const std::exception &e)
+    {
+        return false;
+    }
+    return true;
+}
+bool ccmdstr::getValue(const int index,long &value)
+{
+    this->checkIndex(index);
+    try
+    {
+        value=stol(pickNum(this->vstr[index],true));
+    }
+    catch (const std::exception &e)
+    {
+        return false;
+    }
+    return true;
 }
--- public/stringop/stringop.h
+++ public/stringop/stringop.h
@@ -4,7 +4,8 @@
 #pragma once
 
 #include "../cpublic.h" //c/c++常用头文件，如有新增在此文件中添加
-
+#include <string>
+#include <vector>
 
 /**
  * 删除字符串从左边开始的指定字符，默认缺省为空格
@@ -13,8 +14,8 @@
  * @param ch 要删除的字符
  * @return 处理后的字符串
  */
-char* deleteLChar(char* str,const char ch=' ');
-std::string& deleteLChar(std::string& str,const char ch=' ');
+char *deleteLChar(char *str, const char ch = ' ');
+std::string &deleteLChar(std::string &str, const char ch = ' ');
 
 /**
  *删除字符串从尾部开始的指定字符，默认缺省为空格
@@ -23,8 +24,8 @@ std::string& deleteLChar(std::string& str,const char ch=' ');
  * @param ch 要删除的字符
  * @return 处理后的字符串
  */
-char* deleteRChar(char* str,const char ch=' ');
-std::string& deleteRChar(std::string& str,const char ch=' ');
+char *deleteRChar(char *str, const char ch = ' ');
+std::string &deleteRChar(std::string &str, const char ch = ' ');
 
 /**
  * 删除字符串左右两边的指定字符，默认缺省为空格
@@ -33,21 +34,21 @@ std::string& deleteRChar(std::string& str,const char ch=' ');
  * @param ch 要删除的字符
  * @return  处理后的字符串
  */
-char* deleteChar(char* str,const char ch=' ');
-std::string& deleteChar(std::string& str,const char ch=' ');
+char *deleteChar(char *str, const char ch = ' ');
+std::string &deleteChar(std::string &str, const char ch = ' ');
 
 /**
  * @brief 将字符串中的小写字母转换为大写字母
- * 
+ *
  * @param str 要处理的字符串
  * @return 处理完后的字符串
  */
-char* toUpper(char* str);
-std::string& toUpper(std::string& str);
+char *toUpper(char *str);
+std::string &toUpper(std::string &str);
 
 /**
  * @brief 将字符串的大写字母全部转换为小写字母
- * 
+ *
  * @param str 要处理的字符串
  * @return 处理完后的字符串
  */
@@ -61,8 +62,8 @@ std::string &toLower(std::string &str);
  * @param src 被替换的字符串
  * @param dest 需要替换成的字符串
  * @param isLoop 是否循环替换，默认为false
- * @return true 
- * @return false 
+ * @return true
+ * @return false
  */
 bool replaceStr(char *str, const char *src, const char *dest, const bool isLoop = false);
 bool replaceStr(std::string &str, const std::string &src, const std::string &dest, const bool isLoop = false);
@@ -76,16 +77,79 @@ bool replaceStr(std::string &str, const std::string &src, const std::string &des
  * @param bFloat 是否提取小数点，默认为false
  * @return 存放数字等的字符串
  */
-char* pickNum(const std::string& src,char* dest,const bool bSigned=false,const bool bFloat=false);
+char *pickNum(const std::string &src, char *dest, const bool bSigned = false, const bool bFloat = false);
 std::string &pickNum(const std::string &src, std::string &dest, const bool bSigned = false, const bool bFloat = false);
 std::string pickNum(const std::string &src, const bool bSigned = false, const bool bFloat = false);
 
 /**
  * @brief 正则表达式匹配处理函数
- * 
+ *
  * @param str 需要去匹配的字符串
  * @param pattern 正则表达式匹配规则
- * @return true 
- * @return false 
+ * @return true
+ * @return false
  */
 bool matchstr(const std::string &str, const std::string &pattern);
+
+///////////////////////////  ////////////////////////////
+
+///////////////////////////  ////////////////////////////
+
+// 字符串分割类ccmdstr
+// 用于将字符串按照指定的分隔符（字符串）分割成多个子字符串
+// 字符串的格式为：字段内容1+分隔符+字段内容2+分隔符+字段内容3+分隔符+...+字段内容n。
+// 例如："messi,10,striker,30,1.72,68.5,Barcelona"，这是足球运动员梅西的资料。
+// 包括：姓名、球衣号码、场上位置、年龄、身高、体重和效力的俱乐部，字段之间用半角的逗号分隔。
+class ccmdstr
+{
+  private:
+    std::vector<std::string> vstr; // 存放分割后的字符串
+    // 检查index是否越界
+    bool checkIndex(const int index);
+
+  public:
+    ccmdstr() {};
+    ccmdstr(const ccmdstr &) = delete;
+    ccmdstr &operator=(const ccmdstr &) = delete;
+
+    // 超级构造函数，在构造期间完成字符串的分割
+    ccmdstr(const std::string &src, const std::string &sep, bool isDeleteSpace = false);
+
+    // 重载[]运算符，返回分割后的字符串,像数组一样访问
+    std::string operator[](const int index);
+
+    /**
+     * @brief 核心函数，按照指定的字符串进行分割，然后将结果存放到vstr中
+     *
+     * @param src 要分割的字符串
+     * @param sep 按照什么字符串分割
+     * @param isDeleteSpace 是否删除分割后的字符串两边的空格
+     * @return true
+     * @return false
+     */
+    bool split(const std::string &src, const std::string &sep, bool isDeleteSpace = false);
+
+    // 返回分割后的字符串个数
+    int size();
+
+    /**
+     * @brief 获取分割后字符串的对应类型的值，需要指定下表
+     *
+     * @param index 数组下标
+     * @param value 用来获取结果的值
+     * @return true
+     * @return false
+     */
+    bool getValue(const int index, std::string &value);
+    bool getValue(const int index, char *value, const int len);
+    bool getValue(const int index, int &value);
+    bool getValue(const int index, unsigned int &value);
+    bool getValue(const int index, long &value);
+    bool getValue(const int index, unsigned long &value);
+    bool getValue(const int index, long long &value);
+    bool getValue(const int index, unsigned long long &value);
+    bool getValue(const int index, float &value);
+    bool getValue(const int index, double &value);
+    bool getValue(const int index, long double &value);
+    bool getValue(const int index, bool &value);
+};
